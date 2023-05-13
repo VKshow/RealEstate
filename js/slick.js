e26277ee82fa9d533f9a64d3b6920455e9ca013d
vk <justvk2013@gmail.com>
2023-05-12 18:22:45 -0600

    first commit

---
+++ js/slick.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        loop: true,
+        
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        
+    });
+  });
+
+
