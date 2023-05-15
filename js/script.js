621d9e24fb9d9d2a4daefe65a02bad114f77f4ad
vk <justvk2013@gmail.com>
2023-05-14 22:00:41 -0600

    completed desktop version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
+// filter buttons
+const menuItems = document.querySelectorAll('.features__menu-item');
 
-
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Remove .item_active class from all other menu items
+    menuItems.forEach(item => item.classList.remove('item_active'));
+    
+    // Add .item_active class to the clicked menu item
+    item.classList.add('item_active');
+  });
+});
   
+// slider two-----------------------
+
+
+// Get the slider container and slide items
+const sliderContainer = document.querySelector('.features__content');
+const slideItems = Array.from(document.querySelectorAll('.features__slide'));
+
+// Calculate the slide width
+const slideWidth = slideItems[0].offsetWidth;
+
+// Set the initial position and index
+let currentPosition = 0;
+let currentIndex = 0;
+
+// Function to slide to the given index
+const slideToIndex = (index) => {
+  currentPosition = -index * slideWidth;
+  sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+  currentIndex = index;
+};
+
+// Function to slide to the next index
+const slideNext = () => {
+  if (currentIndex === slideItems.length - 1) {
+    slideToIndex(0);
+  } else {
+    slideToIndex(currentIndex + 1);
+  }
+};
+
+// Function to slide to the previous index
+const slidePrev = () => {
+  if (currentIndex === 0) {
+    slideToIndex(slideItems.length - 1);
+  } else {
+    slideToIndex(currentIndex - 1);
+  }
+};
+
+// Get the control buttons
+const leftButton = document.querySelector('.slider__control .left');
+const rightButton = document.querySelector('.slider__control .right');
+
+// Add event listeners to the buttons
+leftButton.addEventListener('click', slidePrev);
+rightButton.addEventListener('click', slideNext);
+
+
+// slider 3------------------------------------------------------
+
+
--- js/slick.js
+++ js/slick.js
@@ -13,5 +13,23 @@ $(document).ready(function(){
         
     });
   });
+  
+  // slider 4
+  $(document).ready(function () {
+    $('.carousel__inner').slick({
+      centerMode: true,
 
+      arrows: false,
+      speed: 3000,
+      adaptiveHeight: true,
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+
+    });
+  });
 
